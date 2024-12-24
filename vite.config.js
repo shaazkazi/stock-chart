45c6f329d9a756d7342f86e764dd7d9adb789b7d
shaazkazi <n3tn1nj4@proton.me>
2024-12-24 18:45:54 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: '/index.html'
+      }
+    }
   }
 })
